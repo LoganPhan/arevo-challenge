21254fd18ef6a2c3a40f157177b93ced66b83a4a
LoganPhan-PC\LoganPhan <long.phan@infodation.vn>
2020-07-14 22:10:25 +0700

    Add another Sample Data

--- src/sampleData.js
+++ src/sampleData.js
@@ -11,8 +11,7 @@ export const data = [
     start: 100,
     end: 300,
     id: 2
-  }
-  ,
+  },
   {
     start: 240,
     end: 320,
@@ -24,38 +23,8 @@ export const data = [
     id: 4 // overlaps with 2 and 3
   },
   {
-    start: 280,
-    end: 350,
-    id: 5 // overlaps with 4
-  }
-  ,
-  {
     start: 350,
     end: 500,
-    id: 6 // overlaps with 5
-  }
-  ,
-  {
-    start: 380,
-    end: 550,
-    id: 7 // overlaps with 5
-  }
-  ,
-  {
-    start: 380,
-    end: 400,
-    id: 8 // overlaps with 5
-  }
-  ,
-  {
-    start: 400,
-    end: 500,
-    id: 9 // overlaps with 5
-  }
-  ,
-  {
-    start: 400,
-    end: 800,
-    id: 10 // overlaps with 5
+    id: 5 // overlaps with 4
   }
 ];
---
+++ src/sampleData_1.js
@@ -0,0 +1,61 @@
+/* don't edit this file */
+export const totalTrackLength = 500;
+
+export const data = [
+  {
+    start: 0,
+    end: 60,
+    id: 1
+  },
+  {
+    start: 100,
+    end: 300,
+    id: 2
+  }
+  ,
+  {
+    start: 240,
+    end: 320,
+    id: 3 // overlaps with 2
+  },
+  {
+    start: 280,
+    end: 410,
+    id: 4 // overlaps with 2 and 3
+  },
+  {
+    start: 280,
+    end: 350,
+    id: 5 // overlaps with 4
+  }
+  ,
+  {
+    start: 350,
+    end: 500,
+    id: 6 // overlaps with 5
+  }
+  ,
+  {
+    start: 380,
+    end: 550,
+    id: 7 // overlaps with 5
+  }
+  ,
+  {
+    start: 380,
+    end: 400,
+    id: 8 // overlaps with 5
+  }
+  ,
+  {
+    start: 400,
+    end: 500,
+    id: 9 // overlaps with 5
+  }
+  ,
+  {
+    start: 400,
+    end: 800,
+    id: 10 // overlaps with 5
+  }
+];
